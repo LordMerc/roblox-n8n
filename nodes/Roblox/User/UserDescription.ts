ae5f2e6573e79dd11ab6db9c857c6ce4028fb935
Mike Boyd <branden.michael.boyd@gmail.com>
2025-06-04 12:52:41 -0400

    Updated to 1.4.6
    Added user thumbnail generation

--- nodes/Roblox/User/UserDescription.ts
+++ nodes/Roblox/User/UserDescription.ts
@@ -27,6 +27,26 @@ export const userOperations: INodeProperties[] = [
 					},
 				},
 			},
+			{
+				name: 'Generate User Thumbnail',
+				value: 'generate_thumbnail',
+				description: 'Generate a user thumbnail',
+				action: 'Generate user thumbnail',
+				routing: {
+					request: {
+						method: 'GET',
+						url: '=/cloud/v2/users/{{$parameter["userId"]}}:generateThumbnail',
+						headers: {
+							'x-api-key': '={{ $credentials.apiKey }}',
+						},
+						qs: {
+							size: '={{ $parameter["arguments_thumbnail_generation"].size }}',
+							format: '={{ $parameter["arguments_thumbnail_generation"].format }}',
+							shape: '={{ $parameter["arguments_thumbnail_generation"].shape }}',
+						},
+					},
+				},
+			},
 		],
 		default: 'get_user',
 	},
@@ -43,8 +63,110 @@ export const userFields: INodeProperties[] = [
 		displayOptions: {
 			show: {
 				resource: ['user'],
-				operation: ['get_user'],
+				operation: ['get_user', 'generate_thumbnail'],
 			},
 		},
+	},
+	{
+		displayName: 'Query Parameters',
+		name: 'arguments_thumbnail_generation',
+		default: {},
+		type: 'collection',
+		description: "The request's query parameters",
+		displayOptions: {
+			show: {
+				resource: ['user'],
+				operation: ['generate_thumbnail'],
+			},
+		},
+		options: [
+			{
+				displayName: 'Size',
+				name: 'size',
+				type: 'options',
+				options: [
+					{
+						name: '48x48',
+						value: '48',
+					},
+					{
+						name: '50x50',
+						value: '50',
+					},
+					{
+						name: '60x60',
+						value: '60',
+					},
+					{
+						name: '75x75',
+						value: '75',
+					},
+					{
+						name: '100x100',
+						value: '100',
+					},
+					{
+						name: '110x110',
+						value: '110',
+					},
+					{
+						name: '150x150',
+						value: '150',
+					},
+					{
+						name: '180x180',
+						value: '180',
+					},
+					{
+						name: '352x352',
+						value: '352',
+					},
+					{
+						name: '420x420',
+						value: '420',
+					},
+					{
+						name: '720x720',
+						value: '720',
+					},
+				],
+				default: '420',
+				description: 'The size of the thumbnail to generate',
+			},
+			{
+				displayName: 'Format',
+				name: 'format',
+				type: 'options',
+				options: [
+					{
+						name: 'PNG',
+						value: 'PNG',
+					},
+					{
+						name: 'JPEG',
+						value: 'JPEG',
+					},
+				],
+				default: 'PNG',
+				description: 'The format of the thumbnail to generate',
+			},
+			{
+				displayName: 'Shape',
+				name: 'shape',
+				type: 'options',
+				options: [
+					{
+						name: 'Round',
+						value: 'ROUND',
+					},
+					{
+						name: 'Square',
+						value: 'SQUARE',
+					},
+				],
+				default: 'ROUND',
+				description: 'The format of the thumbnail to generate',
+			},
+		],
 	},
 ];
